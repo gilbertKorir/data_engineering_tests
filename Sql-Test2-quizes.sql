b269d870dc6a200504a8527babe644fe57d2f5d7
gilbertKorir <jerikorir@gmail.com>
2025-10-30 20:20:44 +0300

    doc: updated the sql2 test

--- Sql-Test2-quizes.sql
+++ Sql-Test2-quizes.sql
@@ -108,5 +108,67 @@
 	GROUP BY review_score
 	ORDER BY total_reviews DESC;
 
+--4. JOIN + GROUPING Practice
+--Join orders and order_payments to show total payment per order.
+	SELECT o.order_id, SUM(op.payment_value)AS total_payment
+	FROM orders o
+	JOIN order_payments op ON o.order_id = op.order_id
+	GROUP BY o.order_id
+	ORDER BY total_payment DESC;
 
+--Join orders and customers to display each order’s customer city.
+	SELECT c.customer_id, c.customer_city
+	FROM customers c
+	JOIN orders o ON c.customer_id = o.customer_id 
 	
+--Join order_items and products to show the category for each order item.
+	SELECT oi.order_id, p.product_category_name
+	FROM order_items oi
+	JOIN products p ON oi.product_id = p.product_id 
+	
+--Join sellers and geolocation to display each seller’s coordinates.
+	SELECT s.seller_id, g.geolocation_zip_code_prefix AS geolocation
+	FROM sellers s
+	JOIN geolocation g ON s.seller_zip_code_prefix = g.geolocation_zip_code_prefix 
+	
+--Join orders and order_reviews to show the average review score per order status.
+	SELECT o.order_status, AVG(orw.review_score)AS average_score
+	FROM orders o
+	JOIN order_reviews orw ON o.order_id = orw.order_id
+	GROUP BY o.order_status
+--Join order_items and orders to find total freight cost per order status.
+	SELECT o.order_status, SUM(oi.freight_value)AS total_freigth_cost
+	FROM orders o
+	JOIN order_items oi ON o.order_id = oi.order_id 
+	GROUP BY O.order_status
+	ORDER BY total_freigth_cost DESC
+	
+--Join leads_closed and sellers to show leads per seller city.
+	SELECT s.seller_city, COUNT(lc.mql_id) AS leads
+    FROM sellers s
+    JOIN leads_closed lc ON s.seller_id = lc.seller_id
+    GROUP BY s.seller_city
+    ORDER BY leads DESC
+    
+--Join order_items and products to show top 5 most frequently sold categories.
+    SELECT p.product_category_name, COUNT(oi.product_id) AS top_sold_category
+    FROM products p
+    JOIN order_items oi ON p.product_id = oi.product_id  
+    GROUP BY p.product_category_name
+    ORDER BY top_sold_category DESC
+    LIMIT 5;
+    
+--Join leads_closed and leads_qualified to count successful conversions per origin.
+	SELECT lq.origin, COUNT(lc.mql_id) AS successful_conversions
+	FROM leads_qualified lq  
+	JOIN leads_closed lc ON lq.mql_id = lc.mql_id 
+	GROUP BY lq.origin
+	ORDER BY successful_conversions DESC;
+
+--Join orders, order_payments, and customers to find the total amount spent by each customer.
+	SELECT  c.customer_unique_id, SUM(op.payment_value)AS total_spentby_customer
+	FROM customers c
+	JOIN orders o ON c.customer_id = o.customer_id
+	JOIN order_payments op ON o.order_id = op.order_id 
+	GROUP BY c.customer_unique_id
+	ORDER BY total_spentby_customer DESC;
